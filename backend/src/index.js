263b3582b1237408894841d7e38230249c6626ed
abhinavbaral <abhinavbaral@gmail.com>
2025-06-14 10:23:06 +0545

    basic code index

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express ();
+const port = 4000;;
+app.use(express.json);
+app.get("/",(req, res) =>
+{
+    res.send("Hello");
+});
+app.get("/greet",(req, res) =>
+{
+    res.send("Hello. This is Greeting route");
+});
+app.listen(port , () =>
+{console.log(`server is running on http://local host: $(port)`);
+});
